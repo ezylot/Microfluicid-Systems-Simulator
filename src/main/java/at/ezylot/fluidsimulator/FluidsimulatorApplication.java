ad8a5f97d278b8a0d559b88b23dc6ec278b2cc6b
Florian Schöffl <florian.schoeffl@gmail.com>
2019-03-13 10:55:35 +0100

    Initial commit

---
+++ src/main/java/at/ezylot/fluidsimulator/FluidsimulatorApplication.java
@@ -0,0 +1,13 @@
+package at.ezylot.fluidsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluidsimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluidsimulatorApplication.class, args);
+    }
+
+}
