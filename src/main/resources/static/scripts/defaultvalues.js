f69cab48f2dafb9fd645ad274f2ed468cefdb10c
Florian Schöffl <florian.schoeffl@gmail.com>
2019-05-16 18:06:45 +0200

    You can now enter default values

---
+++ src/main/resources/static/scripts/defaultvalues.js
@@ -0,0 +1,15 @@
+let defaultValues = {
+    length: 0,
+    width: 0,
+    height: 0,
+    pressure: 0,
+    volume: 0,
+};
+
+$(document).ready(() => {
+    $('input#default-length').on('change', event => { defaultValues.length = parseInt(event.target.value); });
+    $('input#default-width').on('change', event => { defaultValues.width = parseInt(event.target.value); });
+    $('input#default-height').on('change', event => { defaultValues.height = parseInt(event.target.value); });
+    $('input#default-pressure-pumpValue').on('change', event => { defaultValues.pressure = parseInt(event.target.value); });
+    $('input#default-volume-pumpValue').on('change', event => { defaultValues.volume = parseInt(event.target.value); });
+});
