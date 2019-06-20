5b4669df667abf948d327965884d0920bf62ef1d
Florian Schöffl <florian.schoeffl@gmail.com>
2019-06-20 02:03:30 +0200

    Added more todos

--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -7,7 +7,9 @@
 // TODO: Change mouse pointer depending on selected element in the element palette
 // TODO: Pump renaming test (sometimes something breaks)
 // TODO: Select next entry when deleting (? do not know if really better this way)
-// TODO: names should be unique
+// TODO: fluid names should be unique
+// TODO: droplet names should be unique
+// TODO: pump names should be unique
 // TODO: Open dialog when downloading to let user select download location/name
 // TODO: Change icons of Play/Pause/Next/Previous Buttons
 
